e71c4b4b8d90533cdb157a62e5554925bd3ee117
funador <jesse.heaslip@gmail.com>
2019-10-29 08:40:57 -0600

    initial scaffolding

---
+++ src/assets/colors.ts
@@ -0,0 +1,15 @@
+// Color names provided by colorhexa.com
+const colors = {
+  black: '#000000',
+  darkLimeGreen: '#64A364',
+  lightGrayishLimeGreen: '#e9f9ea',
+  mostlyWhiteGrey: '#f7f7f7',
+  softRed: '#ee4957',
+  veryLightGray: '#e5e5e5',
+  veryDarkGrayishBlue: '#44484c',
+  veryPaleRed: '#ffe4e4',
+  vividRed: '#f20505',
+  white: '#ffffff',
+}
+
+export { colors }
---
+++ src/components/Header.test.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import { shallow } from 'enzyme'
+import Header from './Header'
+
+it('renders without crashing', () => {
+  shallow(<Header />)
+})
