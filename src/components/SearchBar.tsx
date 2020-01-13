0fcec1a3555f5b8beb6d93b88c675996827ee811
justiny2c <justiny2c@gmail.com>
2020-01-13 10:26:16 -0800

    SearchBar styled

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -14,8 +14,8 @@ class SearchBar extends React.Component {
   render() {
     return (
       <Container>
-        <input></input>
-        <button>Search</button>
+        <Input></Input>
+        <Button>Search</Button>
       </Container>
     )
   }
@@ -24,6 +24,28 @@ class SearchBar extends React.Component {
 const Container = styled.div({
   display: 'flex',
   justifyContent: 'space-between',
+  marginTop: '2rem',
+  marginBottom: '2rem',
+})
+
+const Input = styled.input({
+  width: '100%',
+  height: '2rem',
+  backgroundColor: '#f7f7f7',
+  borderRadius: '5px',
+  border: '1px solid #f7f7f7',
+})
+
+const Button = styled.button({
+  border: 'none',
+  borderRadius: '5px',
+  backgroundColor: '#0794e3',
+  color: 'white',
+  fontSize: '0.8rem',
+  width: '5rem',
+  '&:hover': {
+    backgroundColor: '#7d7c7b',
+  },
 })
 
 export default SearchBar
