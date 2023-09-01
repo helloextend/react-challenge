8f9f7d1fcb8ddc939d24b348bb44fe20abc13b62
github-usman <usmantoyou@gmail.com>
2023-09-01 05:51:21 +0530

    logo design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,20 +6,36 @@ const Header: FC = () => {
   return (
     <Container>
       <Title>Dog Breeds</Title>
-      <Heart icon="redHeartIcon" alt="red heart icon" />
+      <div className="icon">
+        <Heart icon="redHeartIcon" alt="red heart icon" />
+      </div>
     </Container>
   )
 }
 
-const Container = styled.div({
-  display: 'flex',
-  justifyContent: 'space-between',
-})
+const Container = styled.div`
+  display: flex;
+  width: 560px;
+  justify-content: space-between;
+  position: absolute;
+  top: 57px;
+  left: 61px;
+  .icon {
+    margin-top: 8px;
+  }
+`
 
-const Title = styled.h1({
-  fontWeight: 'bold',
-  fontSize: '24px',
-  lineHeight: '33px',
-})
+const Title = styled.div`
+  position: relative;
+  font-family: 'Nunito Sans', sans-serif;
+  width: 131px;
+  height: 24px;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 33px;
+  letter-spacing: 0px;
+  text-align: left;
+  margin-bottom: 8px;
+`
 
 export default Header
